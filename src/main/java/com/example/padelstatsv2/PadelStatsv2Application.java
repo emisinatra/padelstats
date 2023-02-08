32db5c8f12d0f3e06fc93c2ecef67a670f20e155
Emiliano Sinatra <58446200+emisinatra@users.noreply.github.com>
2023-02-08 16:18:54 -0300

    first commit

---
+++ src/main/java/com/example/padelstatsv2/PadelStatsv2Application.java
@@ -0,0 +1,13 @@
+package com.example.padelstatsv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadelStatsv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PadelStatsv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/padelstatsv2/PadelStatsv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.padelstatsv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadelStatsv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
